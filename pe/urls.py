df947efb7fe9c85c92b57f5997e916fd4cb9b528
PaoloMontemurro <montep@usi.ch>
2023-12-06 16:09:15 +0100

    -

--- pe/urls.py
+++ pe/urls.py
@@ -5,5 +5,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pe_app.urls')),
-    path('mydashapp/', include('mydashapp.urls')),
-]
+    ]
--- pe_app/urls.py
+++ pe_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dash_page/', views.dash_page, name='dash_page'),
 ]
